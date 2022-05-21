a23135616c2e991e25ff951c5fdd3ce2997422f3
Chandra Shekhar <csahu3932@gmail.com>
2022-05-21 19:25:54 +0530

    FIrst commit

---
+++ sumOfN.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int findSumTillN(int n){
+    if(n==1){
+        return 1;
+    }
+    int remainingSum=n+findSumTillN(n-1);
+    return remainingSum;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<findSumTillN(n)<<endl;
+
+
+    return 0;
+}
