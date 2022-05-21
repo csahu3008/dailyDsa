1e61c6f443c168e6b903f84d50b2e61ea6b0402d
Chandra Shekhar <csahu3932@gmail.com>
2022-05-21 19:56:21 +0530

    DAY 5 PALINDROME STRING

---
+++ isPalindrome.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+bool isPalindromeCheck(int l,int r,string &s)
+{
+	if(l==r || l>r){
+		return true;
+	}
+	if(s[l]!=s[r]){
+		return false;
+	}else{
+		return isPalindromeCheck(l+1,r-1,s);
+	}
+	return false;
+}
+bool isPalindrome(string &s)
+{
+	return isPalindromeCheck(0,s.length()-1,s);
+}
+
+int main(){
+    string s;
+    cin>>s;
+    cout<<isPalindrome(s)<<endl;
+
+
+    return 0;
+}
